277bdd14a7d6eb04a99e6a93b5850e6432920e3e
4nt1 <antoinemary@hotmail.fr>
2014-04-12 13:12:31 +0200

    correction for embedded documents bug

--- lib/localized_files.rb
+++ lib/localized_files.rb
@@ -9,62 +9,67 @@ rescue LoadError => e
 end
 module Mongoid
   module Paperclip
-        module LocalizedFiles
+    module LocalizedFiles
       extend ActiveSupport::Concern
 
       included do
-        self.class_eval do
-          include Mongoid::Paperclip
-          field :localized_files,     type: Hash,   default: {}
+        class << self
+          attr_accessor :localized_file_fields
+        end
 
-          after_find do |that|
-            that.localized_files.each do |field, locales|
-              locales.each do |locale|
-                define_mongoid_method(field, locale)
-              end
+        @localized_file_fields = []
+        include Mongoid::Paperclip
+        field :localized_files,     type: Hash,   default: {}
+
+        after_find do |that|
+          that.localized_files.each do |field, locales|
+            locales.each do |locale|
+              define_mongoid_method(field, locale)
             end
           end
+        end
 
-          def method_missing(meth, *args, &block)
-            arr = "#{meth}".gsub('=', '').split('_').map(&:to_sym)
-            if self.class.localized_file_fields.include?(arr[0])
-              define_mongoid_method(arr[0], arr[1])
-              self.send(meth, *args)
-            else
-              super(meth, *args, &block)
+        def method_missing(meth, *args, &block)
+          arr = "#{meth}".gsub('=', '').split('_').map(&:to_sym)
+          if self.class.localized_file_fields.include?(arr[0])
+            define_mongoid_method(arr[0], arr[1])
+            self.send(meth, *args)
+          else
+            super(meth, *args, &block)
+          end
+        end
+
+
+        def define_mongoid_method(field, locale, options={})
+          self.class_eval do
+            has_mongoid_attached_file("#{field}_#{locale}".to_sym, options)
+            alias_method "#{field}_#{locale}_private=".to_sym, "#{field}_#{locale}=".to_sym
+            alias_method "#{field}_#{locale}_private".to_sym, "#{field}_#{locale}".to_sym
+
+            define_method("#{field}_#{locale}=") do |file|
+              self.send("#{field}_#{locale}_private=".to_sym, file)
+              presence = self.send("#{field}_#{locale}_private").present?
+              update_localized_files_hash(field, locale, presence)
+              file
+            end
+
+            define_method("#{field}_#{locale}") do
+              self.send("#{field}_#{locale}_private".to_sym)
             end
           end
+          self.class.has_mongoid_attached_file("#{field}_#{locale}".to_sym, options)
 
+        end
 
-          def define_mongoid_method(field, locale, options={})
-            self.class_eval do
-              has_mongoid_attached_file("#{field}_#{locale}".to_sym, options)
-              alias_method "#{field}_#{locale}_private=".to_sym, "#{field}_#{locale}=".to_sym
-              alias_method "#{field}_#{locale}_private".to_sym, "#{field}_#{locale}".to_sym
-
-              define_method("#{field}_#{locale}=") do |file|
-                self.send("#{field}_#{locale}_private=".to_sym, file)
-                presence = self.send("#{field}_#{locale}_private").present?
-                update_localized_files_hash(field, locale, presence)
-                file
-              end
-
-              define_method("#{field}_#{locale}") do
-                self.send("#{field}_#{locale}_private".to_sym)
-              end
-            end
-          end
-
-          def update_localized_files_hash(field, locale, presence)
-            locale = locale.to_sym
-            self.localized_files["#{field}"] = [] if self.localized_files["#{field}"].blank?
-            # adding a file
-            if presence
-              self.localized_files["#{field}"].push(locale) if !self.localized_files["#{field}"].include?(locale)
-            # deleting a file
-            elsif !presence
-              self.localized_files["#{field}"].delete(locale) if self.localized_files["#{field}"].include?(locale)
-            end
+        def update_localized_files_hash(field, locale, presence)
+          locale = locale.to_sym
+          self.localized_files["#{field}"] = [] if self.localized_files["#{field}"].blank?
+          # adding a file
+          if presence
+            self.localized_files["#{field}"].push(locale) if !self.localized_files["#{field}"].include?(locale)
+          # deleting a file
+          elsif !presence
+            self.localized_files["#{field}"].delete(locale) if self.localized_files["#{field}"].include?(locale)
           end
         end
       end
@@ -74,15 +79,9 @@ module Mongoid
       end
 
       module ClassMethods
-        @@localized_file_fields = []
-
-        def localized_file_fields
-          @@localized_file_fields
-        end
 
         def has_mongoid_localized_file(field, options = {})
-
-          @@localized_file_fields.push(field) if !@@localized_file_fields.include?(field)
+          localized_file_fields.push(field) if !localized_file_fields.include?(field)
 
           define_method(field) do |locale=I18n.locale|
             define_mongoid_method(field, locale, options)
@@ -90,10 +89,13 @@ module Mongoid
           end
 
           define_method("#{field}=") do |file|
+            locale = I18n.locale
             if file.is_a?(File) || file.nil?
-              locale = I18n.locale
               define_mongoid_method(field, locale, options)
               self.send("#{field}_#{locale}=".to_sym, file)
+              presence = self.send("#{field}_#{locale}_private").present?
+              update_localized_files_hash(field, locale, presence)
+              file
             else
               raise new TypeError("wrong argument type #{file.class} (expected File)")
             end
@@ -104,9 +106,13 @@ module Mongoid
               if (locale.is_a?(Symbol) || locale.is_a?(String)) && (file.is_a?(File) || file.nil?)
                 define_mongoid_method(field, locale, options)
                 self.send("#{field}_#{locale}=".to_sym, file)
-              else
-                klass = (locale.is_a?(Symbol) || locale.is_a?(String)) ? 'Symbol or String' : 'File'
-                raise new TypeError("wrong argument type File (expected #{klass})")
+                presence = self.send("#{field}_#{locale}_private").present?
+                update_localized_files_hash(field, locale, presence)
+                file
+              elsif file.is_a?(File) || file.nil?
+                raise new TypeError("wrong argument type #{locale.klass} (expected Symbol or String)")
+              elsif locale.is_a?(Symbol) || locale.is_a?(String)
+                raise new TypeError("wrong argument type #{file.klass} (expected File)")
               end
               self.localized_files
             end
@@ -119,5 +125,6 @@ module Mongoid
         end
       end
     end
+
   end
 end
