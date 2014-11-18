212a1ca7702330075eaa83039cb7aecdc9f882ac
4nt1 <antoinemary@hotmail.fr>
2014-11-18 15:34:31 +0100

    enclose method definition in a def (inception ^^)

--- lib/localized_files.rb
+++ lib/localized_files.rb
@@ -10,7 +10,6 @@ end
 module LocalizedFiles
   extend ActiveSupport::Concern
   included do
-    # @localized_file_fields = []
     field :localized_files,     type: Hash,   default: {}
   end
 
@@ -20,10 +19,8 @@ module LocalizedFiles
     def define_mongoid_method(field, locale, options={})
       # we're in the instance
       # define the method on the class if not defined
-      self.class_eval do
-        # we're in the instance.class class
-        has_mongoid_attached_file_original("#{field}_#{locale}".to_sym)
-      end if !self.respond_to?("#{field}_#{locale}".to_sym)
+      # debugger
+      self.class.has_mongoid_attached_file_original("#{field}_#{locale}".to_sym) if !respond_to?("#{field}_#{locale}".to_sym)
 
 
       # field_locale = "#{field}_#{locale}"
@@ -41,6 +38,55 @@ module LocalizedFiles
       # rename the :has_mongoid _attached_file method sur we can add features
       alias_method :has_mongoid_attached_file_original, :has_mongoid_attached_file
 
+      def define_instance_methods(field, options)
+
+        define_method "update_localized_files_hash" do |field, locale, presence|
+
+          locale = locale.to_sym
+          self.localized_files["#{field}"] = [] if self.localized_files["#{field}"].blank?
+          # adding a file
+          if presence
+            self.localized_files["#{field}"].push(locale) if !self.localized_files["#{field}"].include?(locale)
+          # deleting a file
+          elsif !presence
+            self.localized_files["#{field}"].delete(locale) if self.localized_files["#{field}"].include?(locale)
+          end
+        end
+
+
+        # define getter
+        define_method(field) do |locale=I18n.locale|
+          define_mongoid_method(field, locale, options)
+          self.send("#{field}_#{locale}".to_sym)
+        end
+
+        # define setter
+        define_method("#{field}=") do |file|
+          locale = I18n.locale
+          define_mongoid_method(field, locale, options)
+          self.send("#{field}_#{locale}=".to_sym, file)
+          presence = self.send("#{field}_#{locale}").present?
+          update_localized_files_hash(field, locale, presence)
+          file
+        end
+
+        # define setter helper
+        define_method("#{field}_translations=") do |hashed_files|
+          hashed_files.each do |locale, file|
+            define_mongoid_method(field, locale, options)
+            self.send("#{field}_#{locale}=".to_sym, file)
+            presence = self.send("#{field}_#{locale}").present?
+            update_localized_files_hash(field, locale, presence)
+            self.localized_files
+          end
+        end
+
+        # define getter helper
+        define_method("#{field}_translations") do
+          self.localized_files["#{field}"]
+        end
+      end
+
       def has_mongoid_attached_file(field, options={})
 
         # if self.localized_file_fields.nil?
@@ -52,63 +98,8 @@ module LocalizedFiles
         if options.try(:[], :localize) == true
           # localized_file_fields.push(field) if !localized_file_fields.include?(field)
           self.class_eval do
-            # we are in the instance.class class
-
-            # define method on instantiation
-            # after_find do |that|
-            #   that.localized_files.each do |field, locales|
-            #     locales.each do |locale|
-            #       define_mongoid_method(field, locale)
-            #     end
-            #   end
-            # end
-
-            def update_localized_files_hash(field, locale, presence)
-
-              locale = locale.to_sym
-              self.localized_files["#{field}"] = [] if self.localized_files["#{field}"].blank?
-              # adding a file
-              if presence
-                self.localized_files["#{field}"].push(locale) if !self.localized_files["#{field}"].include?(locale)
-              # deleting a file
-              elsif !presence
-                self.localized_files["#{field}"].delete(locale) if self.localized_files["#{field}"].include?(locale)
-              end
-            end
-
-
-            # define getter
-            define_method(field) do |locale=I18n.locale|
-              define_mongoid_method(field, locale, options)
-              self.send("#{field}_#{locale}".to_sym)
-            end
-
-            # define setter
-            define_method("#{field}=") do |file|
-              locale = I18n.locale
-              define_mongoid_method(field, locale, options)
-              self.send("#{field}_#{locale}=".to_sym, file)
-              presence = self.send("#{field}_#{locale}").present?
-              update_localized_files_hash(field, locale, presence)
-              file
-            end
-
-            # define setter helper
-            define_method("#{field}_translations=") do |hashed_files|
-              hashed_files.each do |locale, file|
-                define_mongoid_method(field, locale, options)
-                self.send("#{field}_#{locale}=".to_sym, file)
-                presence = self.send("#{field}_#{locale}").present?
-                update_localized_files_hash(field, locale, presence)
-                self.localized_files
-              end
-            end
-
-            # define getter helper
-            define_method("#{field}_translations") do
-              self.localized_files["#{field}"]
-            end
-
+            puts "in class eval in #{self}"
+            define_instance_methods(field, options)
           end
         else
           options.delete(:localize)
@@ -118,4 +109,3 @@ module LocalizedFiles
     end
   end
 end
-
